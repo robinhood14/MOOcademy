9ac92f9fc658e8b5afbff0303b0169eb8be30f59
robinhood14 <35225066+robinhood14@users.noreply.github.com>
2018-02-01 19:51:00 +0100

    Update course.rb

--- Moocademy/app/models/course.rb
+++ Moocademy/app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
 	has_many :lessons, dependent: :destroy #chaque cours peut avoir plusieurs leçons. Si on supprime un cours (le parent), on supprime aussi toutes les leçons.
-	validates :title,  presence: true, length: { maximum: 80 } #titre doit être rempli et faire moins de 80 char
-	validates :description, presence: true, length: { maximum: 500 }
+	validates :title,  presence: true, length: { maximum: 80 } #condition de validité : le titre doit être présent et faire moins de 80 caractères.
+	validates :description, presence: true, length: { maximum: 500 }# condition de validité : la description doit être présente et faire 500 caractères max.
 end
