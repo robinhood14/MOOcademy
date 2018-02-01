3925f4895f303ccc5bf0fb67faa9cf8661594fa2
robinhood14 <35225066+robinhood14@users.noreply.github.com>
2018-02-01 19:53:12 +0100

    versionOK

--- Moocademy/app/models/lesson.rb
+++ Moocademy/app/models/lesson.rb
@@ -1,5 +1,5 @@
 class Lesson < ApplicationRecord
-  belongs_to :course #les leçons dépendent du'un cours
-  validates :title,  presence: true, length: { maximum: 80 } #le  titre ne doit pas être vide et faire au max 80 char
-  validates :body, presence: true
+  belongs_to :course #les leçons appartiennent(dépendent) d'un cours
+  validates :title,  presence: true, length: { maximum: 80 } #condition de validité : le  titre doit être présent et faire au max 80 caractères
+  validates :body, presence: true # condition de validité : le body doit être présent
 end
